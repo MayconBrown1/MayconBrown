2b74b5504a0c47cb2b214c5f7de661b94dd0a7f5
MayconBrown1 <mayconbrown083@gmail.com>
2025-01-29 20:47:39 -0300

    Atualizar o redirect.php

--- test/redirect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Link real que você quer proteger
-$linkReal = 'https://mayconbrown.com.br/tutorial/8tz0zx01ih';
-
-// Redireciona para o link real
-header("Location: $linkReal");
-exit();
-?>
