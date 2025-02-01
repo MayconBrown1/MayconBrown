497daa302572cf86a8431f3f1c01dd8c46d929c3
MayconBrown1 <mayconbrown083@gmail.com>
2025-02-01 02:21:04 -0300

    Update scripts.js

--- cardapio/1/js/scripts.js
+++ cardapio/1/js/scripts.js
@@ -1,27 +1,88 @@
 document.addEventListener("DOMContentLoaded", () => {
     const produtos = {
         Pasteis: [
-            { id: 1, nome: "Pastel de Carne", preco: 10.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne moída, Massa de pastel." },
-            { id: 2, nome: "Pastel de Frango", preco: 12.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Frango desfiado, Massa de pastel." }
+            { id: 1, nome: "Pastel de Carne Moída", preco: 5.99, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne moída, Cebola, Alho, Azeite, Sal, Pimenta-do-reino." },
+            { id: 2, nome: "Pastel de Frango Desfiado", preco: 6.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Frango desfiado, Cebola, Alho, Azeitona, Milho, Sal, Pimenta-do-reino." },
+            { id: 3, nome: "Pastel de Queijo e Presunto", preco: 4.99, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Queijo mussarela, Presunto, Orégano." },
+            { id: 4, nome: "Pastel de Calabresa com Queijo", preco: 6.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Linguiça calabresa, Queijo mussarela, Cebola, Alho." },
+            { id: 5, nome: "Pastel de Camarão com Catupiry", preco: 7.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Camarão, Catupiry, Alho, Cebola, Coentro." },
+            { id: 6, nome: "Pastel de Carne Seca com Abóbora", preco: 6.80, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne seca desfiada, Abóbora, Alho, Cebola, Sal, Pimenta-do-reino." },
+            { id: 7, nome: "Pastel de Frango com Catupiry", preco: 5.90, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Frango desfiado, Catupiry, Alho, Cebola, Sal." },
+            { id: 8, nome: "Pastel de Pizza (Mussarela, Presunto e Tomate)", preco: 6.20, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Queijo mussarela, Presunto, Tomate, Orégano." },
+            { id: 9, nome: "Pastel de Palmito com Queijo", preco: 5.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Palmito, Queijo mussarela, Alho, Cebola, Azeite." },
+            { id: 10, nome: "Pastel de Bacalhau com Azeitonas", preco: 7.20, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Bacalhau desfiado, Azeitonas verdes, Alho, Cebola, Salsa." }
         ],
         Hamburgues: [
-            { id: 3, nome: "Hambúrguer", preco: 15.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hambúrguer, Carne bovina, Queijo cheddar, Alface, Tomate, Molho especial." }
+            { id: 11, nome: "Hambúrguer Clássico", preco: 9.99, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne bovina, Queijo cheddar, Alface, Tomate, Pão de hambúrguer." },
+            { id: 12, nome: "Cheeseburger", preco: 10.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne bovina, Queijo mussarela, Alface, Pão de hambúrguer, Maionese." },
+            { id: 13, nome: "Hambúrguer Bacon", preco: 12.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne bovina, Bacon, Queijo cheddar, Alface, Tomate, Pão de hambúrguer." },
+            { id: 14, nome: "Veggie Burger", preco: 11.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Hambúrguer de grão de bico, Alface, Tomate, Pão integral." },
+            { id: 15, nome: "Hambúrguer Duplo", preco: 14.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne bovina, Queijo cheddar, Bacon, Alface, Tomate, Pão de hambúrguer." },
+            { id: 16, nome: "Hambúrguer com Molho Barbecue", preco: 13.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne bovina, Queijo cheddar, Molho barbecue, Alface, Pão de hambúrguer." },
+            { id: 17, nome: "Hambúrguer de Frango", preco: 11.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Peito de frango, Queijo cheddar, Alface, Pão de hambúrguer, Maionese." },
+            { id: 18, nome: "Hambúrguer de Costela", preco: 15.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne de costela, Queijo cheddar, Alface, Tomate, Molho especial." },
+            { id: 19, nome: "Hambúrguer Picanha", preco: 16.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne de picanha, Queijo prato, Alface, Pão de hambúrguer." },
+            { id: 20, nome: "Hambúrguer com Ovo", preco: 13.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne bovina, Queijo cheddar, Ovo frito, Alface, Tomate, Pão de hambúrguer." }
         ],
         Pizzas: [
-            { id: 4, nome: "Pizza", preco: 30.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Orégano, Pepperoni (opcional)." }
+            { id: 21, nome: "Pizza Margherita", preco: 12.99, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Manjericão, Orégano." },
+            { id: 22, nome: "Pizza Calabresa", preco: 14.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Calabresa, Cebola." },
+            { id: 23, nome: "Pizza Portuguesa", preco: 16.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Presunto, Ovo, Cebola, Azeitonas." },
+            { id: 24, nome: "Pizza de Frango com Catupiry", preco: 17.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Frango desfiado, Catupiry." },
+            { id: 25, nome: "Pizza 4 Queijos", preco: 18.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Queijo gorgonzola, Queijo parmesão, Queijo provolone." },
+            { id: 26, nome: "Pizza Bacon", preco: 15.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Bacon, Cebola." },
+            { id: 27, nome: "Pizza Vegetariana", preco: 16.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Tomate, Abobrinha, Pimentão, Cogumelos." },
+            { id: 28, nome: "Pizza de Peito de Peru", preco: 14.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Peito de peru, Alface." },
+            { id: 29, nome: "Pizza Frango com Bacon", preco: 17.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Frango desfiado, Bacon." },
+            { id: 30, nome: "Pizza de Carne Seca", preco: 19.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Massa de pizza, Molho de tomate, Queijo mussarela, Carne seca desfiada, Cebola." }
         ],
         HotDog: [
-            { id: 5, nome: "Hot Dog", preco: 12.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Ketchup, Mostarda, Maionese, Batata palha." }
+            { id: 31, nome: "Hot Dog Tradicional", preco: 12.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Ketchup, Mostarda, Maionese, Batata palha." },
+            { id: 32, nome: "Hot Dog Especial", preco: 14.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Ketchup, Maionese, Queijo cheddar, Bacon." },
+            { id: 33, nome: "Hot Dog Vegetariano", preco: 13.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha vegetal, Ketchup, Mostarda, Maionese." },
+            { id: 34, nome: "Hot Dog com Frango", preco: 15.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Peito de frango desfiado, Maionese, Ketchup." },
+            { id: 35, nome: "Hot Dog de Carne", preco: 16.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Carne moída, Ketchup, Maionese, Alface." },
+            { id: 36, nome: "Hot Dog com Molho Especial", preco: 17.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Molho especial, Alface, Tomate." },
+            { id: 37, nome: "Hot Dog com Queijo", preco: 13.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Queijo cheddar, Ketchup, Mostarda." },
+            { id: 38, nome: "Hot Dog Bacon", preco: 14.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Bacon, Ketchup, Mostarda." },
+            { id: 39, nome: "Hot Dog com Ovo", preco: 16.50, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Ovo frito, Maionese." },
+            { id: 40, nome: "Hot Dog Supreme", preco: 18.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Pão de hot dog, Salsicha, Queijo cheddar, Bacon, Ketchup, Maionese." }
         ],
         Acai: [
-            { id: 6, nome: "Açaí", preco: 20.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Mel." }
+            { id: 6, nome: "Açaí Romão e Julieta", preco: 20.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Mel, Morango, Leite condensado." },
+            { id: 7, nome: "Açaí Tradicional", preco: 15.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana." },
+            { id: 8, nome: "Açaí Nutella", preco: 25.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Nutella." },
+            { id: 9, nome: "Açaí com Frutas Vermelhas", preco: 22.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Morango, Framboesa." },
+            { id: 10, nome: "Açaí Tropical", preco: 23.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Manga, Abacaxi, Granola." },
+            { id: 11, nome: "Açaí Protein", preco: 28.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Whey protein." },
+            { id: 12, nome: "Açaí Creme de Amendoim", preco: 24.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Creme de amendoim." },
+            { id: 13, nome: "Açaí com Leite Ninho", preco: 26.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Leite Ninho." },
+            { id: 14, nome: "Açaí Chocolate", preco: 27.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Calda de chocolate." },
+            { id: 15, nome: "Açaí com Paçoca", preco: 25.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Açaí, Granola, Banana, Paçoca." }
         ],
         Petiscos: [
-            { id: 7, nome: "Batata Frita", preco: 10.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Batata, Sal, Óleo para fritura." }
+            { id: 16, nome: "Batata Frita", preco: 10.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Batata, Sal, Óleo para fritura." },
+            { id: 17, nome: "Batata Frita com Queijo", preco: 15.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Batata, Queijo cheddar, Óleo para fritura." },
+            { id: 18, nome: "Coxinha", preco: 10.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Frango, Massa, Óleo para fritura." },
+            { id: 19, nome: "Bolinho de Bacalhau", preco: 18.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Bacalhau, Batata, Cebola, Salsinha." },
+            { id: 20, nome: "Mandioca Frita", preco: 12.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Mandioca, Sal, Óleo para fritura." },
+            { id: 21, nome: "Escondidinho de Carne", preco: 20.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Carne moída, Purê de batata." },
+            { id: 22, nome: "Escondidinho de Frango", preco: 22.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Frango desfiado, Purê de batata." },
+            { id: 23, nome: "Anéis de Cebola", preco: 14.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Cebola, Farinha de trigo, Óleo para fritura." },
+            { id: 24, nome: "Frango à Passarinho", preco: 18.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Frango, Alho, Sal, Óleo para fritura." },
+            { id: 25, nome: "Bolinho de Mandioca", preco: 12.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Mandioca, Carne seca, Cebola." }
         ],
         Bebidas: [
-            { id: 8, nome: "Refrigerante", preco: 7.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Água gaseificada, Açúcar, Corantes e aromatizantes." },
-            { id: 9, nome: "Milkshake", preco: 14.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Leite, Sorvete (chocolate, baunilha ou morango), Chantilly (opcional)." }
+            { id: 26, nome: "Refrigerante", preco: 7.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Água gaseificada, Açúcar, Corantes e aromatizantes." },
+            { id: 27, nome: "Suco Natural de Laranja", preco: 8.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Laranja, Açúcar (opcional)." },
+            { id: 28, nome: "Suco de Abacaxi com Hortelã", preco: 9.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Abacaxi, Hortelã, Açúcar (opcional)." },
+            { id: 29, nome: "Milkshake de Chocolate", preco: 14.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Leite, Sorvete de chocolate, Chantilly." },
+            { id: 30, nome: "Milkshake de Morango", preco: 14.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Leite, Sorvete de morango, Chantilly." },
+            { id: 31, nome: "Cerveja", preco: 6.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Malte, Lúpulo, Água." },
+            { id: 32, nome: "Caipirinha", preco: 18.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Cachaça, Limão, Açúcar, Gelo." },
+            { id: 33, nome: "Suco de Melancia", preco: 8.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Melancia, Açúcar (opcional)." },
+            { id: 34, nome: "Refrigerante Diet", preco: 8.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Água gaseificada, Adoçante, Corantes e aromatizantes." },
+            { id: 35, nome: "Suco de Uva", preco: 9.00, img: "https://mayconbrown.com.br/img/svg/icone.svg", ingredientes: "Uva, Açúcar (opcional)." }
         ]
     };
 
