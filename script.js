ed256ec95c995a50d70a76117690971c836d6925
MayconBrown1 <mayconbrown083@gmail.com>
2025-02-19 20:47:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,13 @@ window.onload = function() {
         clientVideo.volume = 0.5; // Ajusta o volume para 50%
     }
 };
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('ServiceWorker registrado com sucesso: ', registration);
+    }).catch((error) => {
+      console.log('Falha ao registrar o ServiceWorker: ', error);
+    });
+  });
+}
