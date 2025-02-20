80adcbdc199112c7afee66b59d043be3650c7390
MayconBrown1 <mayconbrown083@gmail.com>
2025-02-20 00:38:04 -0300

    Update service-worker.js

--- cardapio/1/service-worker.js
+++ cardapio/1/service-worker.js
@@ -1,16 +1,13 @@
 self.addEventListener('install', (event) => {
-    console.log('Service Worker Instalado');
-});
-
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker Ativado');
-});
-
-// Cacheando arquivos para funcionamento offline
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+    event.waitUntil(
+      caches.open('site-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js'
+        ]);
+      })
     );
-});
+  });
+  
