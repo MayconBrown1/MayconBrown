f493b1e830c5e67fc9e44096fcbaba6969b7f9f4
MayconBrown1 <mayconbrown083@gmail.com>
2025-05-06 17:50:49 -0300

    Update scripts.js

--- cardapio/lella-salgados/js/scripts.js
+++ cardapio/lella-salgados/js/scripts.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
             { id: 26, nome: "Refrigerante Dore Cola 250ml", preco: 2.00, img: "https://mayconbrown.com.br/cardapio/1/img/bebidas.webp", ingredientes: "" },
             { id: 26, nome: "Refrigerante Indaiá Laranja 250ml", preco: 2.00, img: "https://mayconbrown.com.br/cardapio/1/img/bebidas.webp", ingredientes: " " },
             { id: 26, nome: "Refrigerante Guaraná 1L", preco: 7.00, img: "https://mayconbrown.com.br/cardapio/img/acabou.png", ingredientes: "" },
-            { id: 27, nome: "Achocolatado Pira Kids 200ml", preco: 3.00, img: "https://mayconbrown.com.br/cardapio/1/img/bebidas.webp", ingredientes: "" },
+            { id: 27, nome: "Achocolatado Pirakids 200ml", preco: 3.00, img: "https://mayconbrown.com.br/cardapio/1/img/bebidas.webp", ingredientes: "" },
         ],
         Guloseimas: [
             { id: 26, nome: "Pastilha Halls Melancia", preco: 2.00, img: "https://mayconbrown.com.br/cardapio/img/Pastilha-Halls-Melancia.webp", ingredientes: "" },
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
         "Liberdade": 15.00,
         "Monte Castelo": 13.00,
         "Nova Esperança": 6.00,
-        "Parque de Exposições": 16.00,
+        "Parque de Exposições": 15.00,
         "Passagem de Areia": 10.00,
         "Rosa dos Ventos": 7.00,
         "Santa Tereza": 8.00,
